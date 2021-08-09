eea8269a0045bdcc545cea61de5fc6de402c10d4
rohit0596 <rohit.raj.0696@gmail.com>
2021-08-09 18:01:18 +0530

    Class Added : Find only duplicate characters of given String.

---
+++ JavaPrograms/src/test/java/com/tyss/StringPrograms/OnlyDuplicateCharatersOfGivenString.java
@@ -0,0 +1,31 @@
+package com.tyss.StringPrograms;
+
+import java.util.LinkedHashSet;
+
+public class OnlyDuplicateCharatersOfGivenString {
+	
+	public static void main(String[] args) {
+String s = "Jharkhand";
+		
+		//Step 1 : Filter out Unique Character from given String and storing in set.
+		//HashSet<Character> set = new HashSet<Character>();
+		LinkedHashSet<Character> set = new LinkedHashSet<Character>();
+		for(int i = 0; i < s.length(); i++) {
+			set.add(s.charAt(i));
+		}
+		
+		//Step 2: Compare set value with given string value
+		for (Character ch : set) {
+			int count = 0;
+			for(int i = 0; i < s.length(); i++) {
+				if(ch == s.charAt(i)) {
+					count++;
+				}
+			}
+		//Print only Duplicate Character which occurred more then one times 
+			if(count>1)
+			System.out.println(ch+" : "+ count);
+		}
+	}
+
+}
