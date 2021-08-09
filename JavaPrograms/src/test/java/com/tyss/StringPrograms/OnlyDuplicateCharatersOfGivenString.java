9535a293350d729afc2087163812670f237a3a95
rohit0596 <rohit.raj.0696@gmail.com>
2021-08-09 18:11:34 +0530

    Class Added : Find only Duplicate Character from given String.

--- JavaPrograms/src/test/java/com/tyss/StringPrograms/OnlyDuplicateCharatersOfGivenString.java
+++ JavaPrograms/src/test/java/com/tyss/StringPrograms/OnlyDuplicateCharatersOfGivenString.java
@@ -3,17 +3,17 @@ package com.tyss.StringPrograms;
 import java.util.LinkedHashSet;
 
 public class OnlyDuplicateCharatersOfGivenString {
-	
+
 	public static void main(String[] args) {
-String s = "Jharkhand";
-		
+		String s = "Jharkhand";
+
 		//Step 1 : Filter out Unique Character from given String and storing in set.
 		//HashSet<Character> set = new HashSet<Character>();
 		LinkedHashSet<Character> set = new LinkedHashSet<Character>();
 		for(int i = 0; i < s.length(); i++) {
 			set.add(s.charAt(i));
 		}
-		
+
 		//Step 2: Compare set value with given string value
 		for (Character ch : set) {
 			int count = 0;
@@ -22,9 +22,9 @@ String s = "Jharkhand";
 					count++;
 				}
 			}
-		//Print only Duplicate Character which occurred more then one times 
+			//Print only Duplicate Character which occurred more then one times 
 			if(count>1)
-			System.out.println(ch+" : "+ count);
+				System.out.println(ch+" : "+ count);
 		}
 	}
 
